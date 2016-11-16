d36d8173f94b537475acd9a0c1fac5f9e0503bc7
Joe Littlejohn <joelittlejohn@gmail.com>
2016-11-16 00:50:03 +0000

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-project-clean "0.1.0"
+(defproject lein-project-clean "0.1.1-SNAPSHOT"
   :description "A lein plugin to tidy/refactor source files in a project via refactor-nrepl middleware"
   :url "https://github.com/joelittlejohn/lein-project-clean"
   :license {:name "Eclipse Public License"

24b53c29aaa8df0ca9b16b13ac4ccee700f79cc6
Joe Littlejohn <joelittlejohn@gmail.com>
2017-12-08 18:12:51 +0000

    Library upgrade

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/joelittlejohn/lein-project-clean"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cider/cider-nrepl "0.14.0"]
+  :dependencies [[cider/cider-nrepl "0.15.1"]
                  [commons-io "2.5"]
-                 [refactor-nrepl "2.2.0"]
+                 [refactor-nrepl "2.3.1"]
                  [rewrite-clj "0.6.0"]]
   :deploy-repositories {"releases" :clojars}
   :eval-in-leiningen true)

96a466e0cd564bc06c565dc0b96acde846d0c09c
Joe Littlejohn <joelittlejohn@gmail.com>
2018-06-12 11:10:01 +0100

    Update cider-nrepl

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/joelittlejohn/lein-project-clean"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cider/cider-nrepl "0.16.0"]
+  :dependencies [[cider/cider-nrepl "0.17.0"]
                  [commons-io "2.6"]
                  [refactor-nrepl "2.3.1"]
                  [rewrite-clj "0.6.0"]]
